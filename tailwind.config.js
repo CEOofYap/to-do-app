0041d2a64807c47785bb6a9cdce905894ae18b2d
CEOofYap <145884089+CEOofYap@users.noreply.github.com>
2025-07-24 23:14:55 +0800

    learning navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ab94d4',
+        secondary: '#7b3176',
+        accent: '#bc58a3',
+        dark: {
+          100: '#0a060f',
+          200: '#120b1e',
+        },
+        light: {
+          100: '#e7e0f3',
+          200: '#f4f0f9'
+        }
+      }
+    }
   },
   plugins: [],
 }
